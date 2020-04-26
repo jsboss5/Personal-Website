1173dabf1752a863e5dfb1b25245b58c78e1c813
Joshua Boss <joshua.boss@duke.edu>
2020-04-26 16:48:21 -0400

    CSS classes working

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -7,14 +7,15 @@ import { render } from '@testing-library/react';
 class Homepage extends Component {
   render() {
   return (
-    <div className="container-fluid">   
+    <div  class="homePage">   
 
     <h1>
-        Home Page Content
+        Bio
     </h1>
 
     <p>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+   My name is Joshua Boss, and I'm sophomore at Duke studying Computer Science, Mathematics and Literature. I am interested in persuing a career in either full stack software engineering or Product Management. I created this webpage using react - I am trying to improve my front end abilities. Thanks for visiting!
+
     </p>
 
     </div>   
